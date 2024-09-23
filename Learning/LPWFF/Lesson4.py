2dc21b9e7d3168dd6bf51bfe4db5c56048aa4f96
steveb33 <steven32b@gmail.com>
2024-09-23 22:08:04 +0100

    Started lesson 4 which revolves around summarizing the prior 3 lessons and teaches some less important techniques relating to these lessons

---
+++ Learning/LPWFF/Lesson4.py
@@ -0,0 +1,7 @@
+#Lessons 1 though 3 review
+
+#split() can be useful when you cant be bothered for doing quotes for every individual element of a string
+the_string = 'Lamar Jackson rushed for 10 yards'
+print(the_string.split(' '))
+print(' '.join(['Lamar', 'Jackson', 'rushed', 'for', '10', 'yards']))
+
