073dcd4bb878220a126fa783f360c4d3a707deb3
steveb33 <steven32b@gmail.com>
2024-10-03 11:47:33 +0100

    Started learning API connections through python

---
+++ Learning/Random/Bologna/LearnAPI.py
@@ -0,0 +1,32 @@
+"""
+This example script is to figure out how to get stock data via API.
+"""
+import pandas as pd
+import requests
+
+# My API key - find key at https://site.financialmodelingprep.com/developer/docs/dashboard
+api_key = 'RIUapBl1uPO3IVyMCUMzq6R81D55c4iz'
+
+# Some example stocks
+stocks = ['AMZN', 'AAPL', 'META', 'GOOGL']
+
+# Endpoint to get the quote for Apple (AAPL)
+url = f'https://financialmodelingprep.com/api/v3/quote/{",".join(stocks)}?apikey={api_key}'
+
+# Fetch the sample data
+response = requests.get(url)
+
+# Check if response is OK (status code 200)
+if response.status_code == 200:
+    data = response.json()
+
+    # Extract the symbol, price, and volume
+    stock_data = [{'symbol': stock['symbol'], 'price': stock['price'], 'volume': stock['volume']} for stock in data]
+
+    # Create a df with the extracted data
+    df = pd.DataFrame(stock_data)
+    print(df)
+
+
+else:
+    print(f"Error fetching data: {response.status_code}")
