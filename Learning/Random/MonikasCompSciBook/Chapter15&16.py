ceff83108dcb1a04228efea71c080bded0ed8ac2
steveb33 <steven32b@gmail.com>
2024-10-18 13:41:28 +0100

    Yes and yes

--- Learning/Random/MonikasCompSciBook/Chapter15&16.py
+++ Learning/Random/MonikasCompSciBook/Chapter15&16.py
@@ -1,2 +1,9 @@
-''' Chapter 15 - Classes and objects '''
+import math
+
+# 15.1 - Distance between 2 points
+def distance_between_points(point1, point2):
+    distance = math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+    print(distance)
+
+distance_between_points((5.0, 16.0), (3.0, 4.0))
 
