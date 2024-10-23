77c3642560951cdd56c4e9ca4495c958047265c1
steveb33 <steven32b@gmail.com>
2024-10-23 17:19:47 +0100

    Fixing the formatting

--- Learning/Random/MonikasCompSciBook/Chapter15&16.py
+++ Learning/Random/MonikasCompSciBook/Chapter15&16.py
@@ -1,76 +1,76 @@
 import math
 import copy
 
-# # 15.1 - Distance between 2 points
-# def distance_between_points(point1, point2):
-#     distance = math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
-#     print(distance)
-#
-# distance_between_points((5.0, 16.0), (3.0, 4.0))
-#
-# class Point(object):
-#     """Represents a point in 2D space"""
-#
-# class Rectange(object):
-#     """Represents a rectangle
-#
-#     attributes: width, height, corner
-#     """
-# def print_point(p):
-#     print('(%g, %g)' % (p.x, p.y))
-#
-# box = Rectange()
-# box.width = 100.0
-# box.height = 200.0
-#
-# box.corner = Point()
-# box.corner.x = 0.0
-# box.corner.y = 0.0
-#
-# def find_center(rect):
-#     p = Point()
-#     p.x = rect.corner.x + rect.width/2.0
-#     p.y = rect.corner.y + rect.height/2.0
-#     return p
-#
-# center = find_center(box)
-# print_point(center)
-#
-# # Modifying objects - growing a rectangle
-# def grow_rectangle(rect, dwidth, dheight):
-#     rect.width += dwidth
-#     rect.height += dheight
-# print(f'Original width and height: ({box.width}, {box.height})')
-# grow_rectangle(box, 50, 100)
-# print(f'New width and height: ({box.width}, {box.height})')
+# 15.1 - Distance between 2 points
+def distance_between_points(point1, point2):
+    distance = math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+    print(distance)
 
-# # 15.2 - Chaninging the location of an object
-# def move_rectangle(rect, dx, dy):
-#     rect.corner.x += dx
-#     rect.corner.y += dy
-#     return rect
-# print(box.corner.x, box.corner.y)
-# move_rectangle(box, 5, 10)
-# print(box.corner.x, box.corner.y)
-#
-# # Text notes contd
-# box2 = copy.copy(box)
-# print(box2.corner is box.corner)
-#
-# # 15.3 - Deepcopy version of move rectangle
-# def move_rectangle(rect, dx, dy):
-#     new_rect = copy.deepcopy(rect)
-#     new_rect.corner.x += dx
-#     new_rect.corner.y += dy
-#     return new_rect
-#
-# """ Test the original and new rectangle """
-# print(f"Original Rectangle Corner: ({box.corner.x}, {box.corner.y})")
-# moved_box = move_rectangle(box, 5, 10)
-# print(f"Moved Rectangle Corner: ({moved_box.corner.x}, {moved_box.corner.y})")
-# print(f"Original Rectangle Corner After Move: ({box.corner.x}, {box.corner.y})")
-#
-# Chapter 16 Starts Here
+distance_between_points((5.0, 16.0), (3.0, 4.0))
+
+class Point(object):
+    """Represents a point in 2D space"""
+
+class Rectange(object):
+    """Represents a rectangle
+
+    attributes: width, height, corner
+    """
+def print_point(p):
+    print('(%g, %g)' % (p.x, p.y))
+
+box = Rectange()
+box.width = 100.0
+box.height = 200.0
+
+box.corner = Point()
+box.corner.x = 0.0
+box.corner.y = 0.0
+
+def find_center(rect):
+    p = Point()
+    p.x = rect.corner.x + rect.width/2.0
+    p.y = rect.corner.y + rect.height/2.0
+    return p
+
+center = find_center(box)
+print_point(center)
+
+# Modifying objects - growing a rectangle
+def grow_rectangle(rect, dwidth, dheight):
+    rect.width += dwidth
+    rect.height += dheight
+print(f'Original width and height: ({box.width}, {box.height})')
+grow_rectangle(box, 50, 100)
+print(f'New width and height: ({box.width}, {box.height})')
+
+# 15.2 - Chaninging the location of an object
+def move_rectangle(rect, dx, dy):
+    rect.corner.x += dx
+    rect.corner.y += dy
+    return rect
+print(box.corner.x, box.corner.y)
+move_rectangle(box, 5, 10)
+print(box.corner.x, box.corner.y)
+
+# Text notes contd
+box2 = copy.copy(box)
+print(box2.corner is box.corner)
+
+# 15.3 - Deepcopy version of move rectangle
+def move_rectangle(rect, dx, dy):
+    new_rect = copy.deepcopy(rect)
+    new_rect.corner.x += dx
+    new_rect.corner.y += dy
+    return new_rect
+
+""" Test the original and new rectangle """
+print(f"Original Rectangle Corner: ({box.corner.x}, {box.corner.y})")
+moved_box = move_rectangle(box, 5, 10)
+print(f"Moved Rectangle Corner: ({moved_box.corner.x}, {moved_box.corner.y})")
+print(f"Original Rectangle Corner After Move: ({box.corner.x}, {box.corner.y})")
+
+Chapter 16 Starts Here
 class Time(object):
     """Represents the time of day
 
