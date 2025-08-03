fa614b505e9b1ec1910c70063acb1cbf68c4a83d
steveb33 <steven32b@gmail.com>
2025-08-03 13:23:33 +0100

    Starting to log my 100 days coursework on git for good practice. Needed to refresh my skills and figured why not start from the ground up. Going to be doing this daily before returning to machine learning content in order to approach it with a broader perspective on python programming.

---
+++ Learning/100Days/Day_16.py
@@ -0,0 +1,17 @@
+"""Day 16 - Object Oriented Programming (OOP)"""
+
+from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape('turtle')
+# timmy.color('Darkorchid1')
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# timmy.forward(100)
+#
+#
+# my_screen.exitonclick()
+
+import prettytable
